32b47ddbb59aea3e4ac1ff7b94d216c6eee3fe0c
dianannperla <waafirmelyza@hotmail.com>
2025-09-01 16:45:18 +0100

    feat: initial commit for src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Chào mừng bạn đến với Trang Web Phương!'
+    };
+  }
+};
+</script>
+
+<style scoped>
+#app {
+  text-align: center;
+  padding: 20px;
+}
+</style>
